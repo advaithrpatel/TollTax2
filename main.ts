0388c507fb24ce1604092f5907e1fa966c311197
advaithrpatel <85830392+advaithrpatel@users.noreply.github.com>
2023-05-30 17:23:33 +0530

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,19 +8,24 @@ basic.forever(function () {
     makerbit.showStringOnLcd1602("" + (Math.round(makerbit.getUltrasonicDistance(DistanceUnit.CM))), makerbit.position1602(LcdPosition1602.Pos1), 32, TextOption.AlignLeft)
     basic.pause(1000)
     if (makerbit.getUltrasonicDistance(DistanceUnit.CM) <= 20) {
-        if (Toll >= 120) {
+        if (Toll >= 50) {
             Toll = Toll - 50
             BankBalance2 += 50
+            servos.P0.setAngle(180)
             makerbit.showStringOnLcd1602("Valid tag. Have a great journey! :D", makerbit.position1602(LcdPosition1602.Pos1), 32)
             basic.pause(1000)
             makerbit.showStringOnLcd1602("" + (Toll), makerbit.position1602(LcdPosition1602.Pos1), 32, TextOption.AlignLeft)
+            basic.pause(1000)
+            makerbit.showStringOnLcd1602("" + (BankBalance2), makerbit.position1602(LcdPosition1602.Pos1), 32)
             I2C_LCD1602.clear()
             basic.pause(1000)
         } else {
             makerbit.showStringOnLcd1602("Invalid tag. Please recharge. :(", makerbit.position1602(LcdPosition1602.Pos1), 32)
+            basic.pause(2000)
             Toll += 950
             BankBalance += -1000
             BankBalance2 += 50
         }
     }
+    servos.P0.setAngle(90)
 })
